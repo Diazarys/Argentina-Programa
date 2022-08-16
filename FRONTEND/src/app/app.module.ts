1e9246c7f04290d106ec50a089fc5d2997f12d8f
Gaston <diazarys.1@gmail.com>
2022-08-16 15:39:05 -0300

    agregados los interceptores y los ultimos componentes del front 16/8

--- FRONTEND/src/app/app.module.ts
+++ FRONTEND/src/app/app.module.ts
@@ -14,6 +14,10 @@ import { EducacionComponent } from './components/educacion/educacion.component';
 import { NgCircleProgressModule } from 'ng-circle-progress';
 import { ProyectoComponent } from './components/proyecto/proyecto.component';
 import {HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { FormsModule } from '@angular/forms';
+import { interceptorProvider } from './service/interceptor-service';
 
 @NgModule({
   declarations: [
@@ -26,16 +30,22 @@ import {HttpClientModule } from '@angular/common/http';
     AcercaDeComponent,
     ExperienciaComponent,
     EducacionComponent,
-    ProyectoComponent
+    ProyectoComponent,
+    HomeComponent,
+    LoginComponent
+    
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     NgCircleProgressModule.forRoot({}),
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
 
     ],
-  providers: [],
+  providers: [interceptorProvider],
+
   bootstrap: [AppComponent]
 })
 export class AppModule { }
