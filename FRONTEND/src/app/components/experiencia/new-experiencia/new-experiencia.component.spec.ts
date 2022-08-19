f93eae0312574c866d19637b5a3908a2a0c27a81
Gaston <diazarys.1@gmail.com>
2022-08-19 16:24:10 -0300

    Estilos y componentes agregados

--- FRONTEND/src/app/components/experiencia/new-experiencia/new-experiencia.component.spec.ts
+++ FRONTEND/src/app/components/experiencia/new-experiencia/new-experiencia.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { EducacionComponent } from './educacion.component';
+import { NewExperienciaComponent } from './new-experiencia.component';
 
-describe('EducacionComponent', () => {
-  let component: EducacionComponent;
-  let fixture: ComponentFixture<EducacionComponent>;
+describe('NewExperienciaComponent', () => {
+  let component: NewExperienciaComponent;
+  let fixture: ComponentFixture<NewExperienciaComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ EducacionComponent ]
+      declarations: [ NewExperienciaComponent ]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(EducacionComponent);
+    fixture = TestBed.createComponent(NewExperienciaComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
